100ccc7e8985c92ce0f49227c03d178558d54f2a
Vincent james <Vincent James Beato>
2023-09-03 21:36:14 +0800

    test

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -5,8 +5,52 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
         <title>{{ $title ?? 'Page Title' }}</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     </head>
     <body>
-        {{ $slot }}
+
+        <nav class="navbar navbar-light bg-success sticky-top">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">
+                    <img src="{{ asset('images/logo.png') }}" alt="" width="30" height="24" class="d-inline-block align-text-top">
+                    ISPB
+                </a>
+            </div>
+        </nav>
+        
+        @auth
+            <div class="p-1 mt-3">
+                <div class="row w-100" style="height: 100vh;">
+                    <div class="col-3 border border-danger fixed-top">
+                        <ul class="nav flex-column">
+                            <li class="nav-item">
+                                <a class="nav-link active" aria-current="page" href="#">Active</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Link</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Link</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="col border border-danger scrollable">
+                    {{ $slot }}
+                    </div>
+                </div>
+            </div>
+        @endauth
+
+        @guest
+            <div class="col border border-danger">
+                {{ $slot }}
+            </div>
+        @endguest
+
     </body>
 </html>
--- resources/views/livewire/auth/login.blade.php
+++ resources/views/livewire/auth/login.blade.php
@@ -1,3 +1,30 @@
 <div>
-    This is login page 123
+    <div class="my-5">
+        <div class="row gx-5">
+            <div class="col">
+                <div class="p-3 border bg-light">Custom column padding</div>
+            </div>
+            
+            <div class="col">
+                <div class="p-3 border rounded-start bg-light">
+                    <h1>L<small>OGIN</small></h1>
+
+                    <form class="form">
+                        <div class="mb-3 w-50">
+                            <label for="formGroupExampleInput" class="form-label">Email</label>
+                            <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Example input placeholder">
+                        </div>
+
+                        <div class="mb-3 w-50">
+                            <label for="formGroupExampleInput2" class="form-label">Password</label>
+                            <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Another input placeholder">
+                        </div>
+
+                        <button class="btn btn-primary w-50" type="submit">Submit form</button>
+                    </form>
+
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
