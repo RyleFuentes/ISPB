550573f0672019efd4c8d7a845e3fc2fd7820ebb
Vincent james <Vincent James Beato>
2023-09-09 22:11:17 +0800

    login pa lng

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -9,76 +9,66 @@
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     </head>
-    <body >
+    <body>
 
-        <nav class="navbar  navbar-expand-lg bg-body-tertiary" >
-            <div class="container-fluid">
-              <a class="navbar-brand" href="#">ISPB</a>
-              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-              </button>
-              <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                  <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Home</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
-                  </li>
-                  <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                      Dropdown
-                    </a>
-                    <ul class="dropdown-menu">
-                      <li><a class="dropdown-item" href="#">Action</a></li>
-                      <li><a class="dropdown-item" href="#">Another action</a></li>
-                      <li><hr class="dropdown-divider"></li>
-                      <li><a class="dropdown-item" href="#">Something else here</a></li>
-                    </ul>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-                  </li>
-                </ul>
-                <form class="d-flex" role="search">
-                  <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                  <button class="btn btn-outline-success" type="submit">Search</button>
-                </form>
-              </div>
+      <div style="background-image: url('{{ asset('images/Sprinkle.svg') }}'); background-size: cover; background-repeat: no-repeat; background-position: center center; height: 100vh;">
+        <nav class="navbar navbar-expand-lg bg-dark shadow sticky-top">
+          <div class="container-fluid">
+            <a class="navbar-brand" href="#">ISPB</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                  <a class="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Link</a>
+                </li>
+              </ul>
+              <ul class="navbar-nav d-flex" role="search">
+                <li class="nav-item">
+                  <a class="nav-link" href="#">JonahLynn</a>
+                </li>
+              </ul>
             </div>
-          </nav>
+          </div>
+        </nav>
+
         @auth
-            <div class="p-1 mt-3">
-                <div class="row w-100" style="height: 100vh;">
-                    <div class="col-3 border  fixed-top">
-                        <ul class="nav flex-column">
-                            <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="#">Active</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Link</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Link</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-                            </li>
-                            <livewire:components.logout />
-                        </ul>
-                    </div>
-                    <div class="col border border-danger scrollable">
-                    {{ $slot }}
-                    </div>
-                </div>
-            </div>
+          <div class="p-1 mt-3">
+              <div class="row w-100" style="height: 100vh;">
+                  <div class="col-3 border  fixed-top">
+                      <ul class="nav flex-column">
+                          <li class="nav-item">
+                              <a class="nav-link active" aria-current="page" href="#">Active</a>
+                          </li>
+                          <li class="nav-item">
+                              <a class="nav-link" href="#">Link</a>
+                          </li>
+                          <li class="nav-item">
+                              <a class="nav-link" href="#">Link</a>
+                          </li>
+                          <li class="nav-item">
+                              <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                          </li>
+                          <livewire:components.logout />
+                      </ul>
+                  </div>
+                  <div class="col border border-danger scrollable">
+                  {{ $slot }}
+                  </div>
+              </div>
+          </div>
         @endauth
 
         @guest
-            <div class="container">
-                {{ $slot }}
-            </div>
+          <div class="container position-absolute top-50 start-50 translate-middle">
+              {{ $slot }}
+          </div>
         @endguest
+      </div>
 
     </body>
 </html>
--- resources/views/livewire/auth/login.blade.php
+++ resources/views/livewire/auth/login.blade.php
@@ -1,10 +1,10 @@
-<div class="">
-    <div class="container-fluid mt-5  d-flex align-items-center" style="justify-content: space-between">
-        <div class="container">
-            Hello world
+<div class="" style="">
+    <div class="d-flex shadow-lg">
+        <div class="container rounded p-5 border border-dark border-end-0 text-light" style="backdrop-filter: blur(5px); height: 50vh;">
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur architecto doloribus, perspiciatis fuga incidunt repellat earum accusamus, ab cumque tempora maiores sit quibusdam exercitationem aperiam accusantium. Delectus eaque tempora pariatur.</p>
         </div>
-        <div class="container rounded p-3" style="background-color: hsl(0, 0%, 0%, .3)">
-            <h3 class='text-center'>LOGIN</h3>
+        <div class="container rounded p-5 border border-dark border-start-0 d-flex flex-column justify-content-center align-content-center" style="backdrop-filter: blur(5px); height: 50vh;">
+            <h3 class='text-center text-light'>LOGIN</h3>
             <form wire:submit='authenticate' class="form">
                 
                 <div class="form-floating mt-3">
@@ -22,8 +22,8 @@
                     <span class="text-danger">{{$message}}</span>
                 @enderror
 
-                <div class="form-group  mt-3">
-                    <button class="btn w-100 btn-outline-dark">Login</button>
+                <div class="form-group mt-3">
+                    <button class="btn w-100 btn-outline-dark text-light">Login</button>
                 </div>
             </form>
         </div>
