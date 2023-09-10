4bdd63dc8eaae46b34194111581b2cc97496bcda
Vincent james <Vincent James Beato>
2023-09-10 20:12:47 +0800

    ok

--- resources/views/livewire/auth/register.blade.php
+++ resources/views/livewire/auth/register.blade.php
@@ -1,9 +1,9 @@
 <div class="">
     <div class="d-flex shadow-lg" style="justify-content: space-between; flex-direction:row-reverse">
-        <div class="container rounded p-5 border border-dark border-start-0 text-light" style="backdrop-filter: blur(7px); height: 70vh;">
+        <div class="container rounded p-5 border border-dark border-start-0 text-light" style="backdrop-filter: blur(7px); height: 00vh;">
             <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Labore nam maxime harum? Soluta iure doloribus dolores minima est cum accusamus quibusdam inventore cupiditate reprehenderit, molestias nobis dolorem excepturi, corrupti hic.</p>
         </div>
-        <div class="container rounded p-5 border border-dark border-end-0 d-flex flex-column justify-content-center align-content-center" style="backdrop-filter: blur(7px); height: 70vh;">
+        <div class="container rounded p-5 border border-dark border-end-0 d-flex flex-column justify-content-center align-content-center" style="backdrop-filter: blur(7px); height: 50vh;">
             <h3 class='text-light display-2'>R<small>EGISTER</small></h3>
             <br>
             <form wire:submit='register'class="form">
@@ -21,20 +21,26 @@
                 @error('email')
                     <span class="text-danger">{{$message}}</span>
                 @enderror
-                <div class="form-floating mt-3">
-                    <input type="password" id="password" wire:model.live='password' placeholder="" class="form-control">
-                    <label for="password">Password</label>
+                <div class="d-flex justify-content-between">
+                    <div class="flex-column">
+                        <div class="form-floating mt-3 flex-fill pe-1">
+                            <input type="password" id="password" wire:model.live='password' placeholder="" class="form-control">
+                            <label for="password">Password</label>
+                        </div>
+                        @error('password')
+                            <span class="text-danger">{{$message}}</span>
+                        @enderror
+                    </div>
+                    <div class="flex-column">
+                        <div class="form-floating mt-3 flex-fill ps-1">
+                            <input type="password" wire:model.live='password_confirmation' id="pass_con" placeholder="" class="form-control bg-primary text-white">
+                            <label for="pass_con">Confirm Password</label>
+                        </div>
+                        @error('password_confirmation')
+                            <span class="text-danger">{{$message}}</span>
+                        @enderror
+                    </div>
                 </div>
-                @error('password')
-                    <span class="text-danger">{{$message}}</span>
-                @enderror
-                <div class="form-floating mt-3">
-                    <input type="password" wire:model.live='password_confirmation' id="pass_con" placeholder="" class="form-control bg-primary text-white">
-                    <label for="pass_con">Confirm Password</label>
-                </div>
-                @error('password_confirmation')
-                    <span class="text-danger">{{$message}}</span>
-                @enderror
 
                 <div class="form-group  mt-3">
                     <button class="btn w-100 btn-outline-light text-light">Login</button>
