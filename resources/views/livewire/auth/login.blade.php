c96f6a01967cb41bb71068ccbfad699d6ff85694
oliverkean <20215296@s.ubaguio.edu>
2023-10-04 09:56:20 +0800

    test

--- resources/views/livewire/auth/login.blade.php
+++ resources/views/livewire/auth/login.blade.php
@@ -1,16 +1,17 @@
 <div class="container d-flex align-items-center justify-content-center" style="height: 100vh">
-    <div class="rounded border border-dark shadow p-5" style="width: 100%">
-        <div class="row">
-            <div class="col">
-                
-            </div>
-            <div class="col">
-                <div class="row">
-                    <h3 class='text-dark fw-bold'>LOGIN</h3>         
+    <div class="rounded shadow-lg h-50 p-5" style="width: 80%;">
+        <div class="row g-5 d-flex align-items-center justify-content-center">
+            <div class="col col-md-6">
+                <div class="text-end">
+                    <h1 class="fw-bold">KHIM <br>EBRAM'S</h1>
+                    <p>Inventory System for Poultry Business</p>
                 </div>
-                <div class="row">
+            </div>
+            <div class="col col-md-6">
+                <div>
+                    <h3 class='text-dark fw-bold'>LOGIN</h3>         
                     <form wire:submit='authenticate' class="form">
-                        
+                            
                         <div class="form-floating mt-3">
                             <input type="text" wire:model='email' id="email" placeholder="name" class="form-control">
                             <label for="email">Email</label>
@@ -35,17 +36,3 @@
         </div>
     </div>    
 </div>
-
-
-{{-- <div class="" style="">
-    <div class="d-flex shadow-lg">
-        <div class="container rounded p-5 border border-dark border-end-0 text-light" style="backdrop-filter: blur(7px); height: 70vh;">
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur architecto doloribus, perspiciatis fuga incidunt repellat earum accusamus, ab cumque tempora maiores sit quibusdam exercitationem aperiam accusantium. Delectus eaque tempora pariatur.</p>
-        </div>
-        <div class="container rounded p-5 border border-dark border-start-0 d-flex flex-column justify-content-center align-content-center" style="backdrop-filter: blur(7px); height: 70vh;">
-        <br>    
-        <h3 class='text-light display-2'>L<small>OGIN</small></h3>
-           
-        </div>
-    </div>
-</div> --}}
