4ff791038e75cb3246ebd90b5a46170143a99d94
Vincent james <Vincent James Beato>
2023-10-08 12:02:36 +0800

    users table responsive

--- resources/views/livewire/pages/users.blade.php
+++ resources/views/livewire/pages/users.blade.php
@@ -2,7 +2,6 @@
     <div class="container rounded p-3 " style="background: hsl(0, 0%, 0%, .7)">
         <div class="container-fluid d-flex ">
             <div class="container">
-
                 <form>
                     <div class="form-floating w-50">
                         <input type="text" id="search" class="form-control " placeholder="...">
@@ -10,88 +9,79 @@
                     </div>
                 </form>
             </div>
-
-            <div class="container d-flex align-items-center " style="justify-content: end">
-                <button class="btn btn-outline-light" data-bs-toggle="modal" data-bs-target="#addBrand"><i
-                        class="bi bi-patch-plus-fill"></i> Brands</button>
-                <button class="btn btn-outline-light m-2" wire:click='addProductMode'><i
-                        class="bi bi-patch-plus-fill"></i> Products</button>
-            </div>
         </div>
     </div>
 
     <div class="container mt-2 rounded p-3 ">
-        <table class="table">
+        <div class="table-responsive-sm">
+            <table class="table">
+                <thead>
+                    <tr class="text-center">
+                        <th scope="col">ID</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">Role</th>
+                        <th scope="col">Actions</th>
+                    </tr>
+                </thead>
+                <tbody class="text-center">
+                    @foreach ($users as $user)
+                        @if ($editing == true && $user->id == $editing_id)
+                            <tr>
+                                <form>
+                                    <th scope="row">{{ $user->id }}</th>
 
-            <thead>
-                <tr>
-                    <th scope="col">ID</th>
-                    <th scope="col">Name</th>
-                    <th scope="col">Email</th>
-                    <th scope="col">Role</th>
-                    <th scope="col">Actions</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($users as $user)
-                    @if ($editing == true && $user->id == $editing_id)
-                        <tr>
-                            <form >
-                                <th  scope="row">{{ $user->id }}</th>
-
-                                <td><input wire:model='edit_name' type="text" class="form-control"
-                                        value="{{ $user->name }}"></td>
-                                @error('edit_name')
-                                    <span class="text-danger">{{ $message }}</span>
-                                @enderror
-                                <td><input wire:model='edit_email' type="email" class="form-control"
-                                        value="{{ $user->email }}"></td>
-                                @error('edit_email')
-                                    <span class="text-danger">{{ $message }}</span>
-                                @enderror
+                                    <td><input wire:model='edit_name' type="text" class="form-control"
+                                            value="{{ $user->name }}"></td>
+                                    @error('edit_name')
+                                        <span class="text-danger">{{ $message }}</span>
+                                    @enderror
+                                    <td><input wire:model='edit_email' type="email" class="form-control"
+                                            value="{{ $user->email }}"></td>
+                                    @error('edit_email')
+                                        <span class="text-danger">{{ $message }}</span>
+                                    @enderror
+                                    <td>
+                                        <select class="form-select" aria-label="Default select example"
+                                            wire:model='edit_role'>
+                                            <option value="0" {{ $edit_role == 0 ? 'selected' : '' }}>Admin</option>
+                                            <option value="1" {{ $edit_role == 1 ? 'selected' : '' }}>Inventory Clerk
+                                            </option>
+                                        </select>
+                                    </td>
+                                    @error('edit_role')
+                                        <span class="text-danger">{{ $message }}</span>
+                                    @enderror
+                                    <td class="d-flex justify-content-evenly">
+                                        <button wire:click='cancel_edit' class="btn btn-warning mx-1">Cancel</button>
+                                        <button wire:click='save_edit({{$user->id}})' class="btn btn-success mx-1">Save</button>
+                                    </td>
+                                </form>
+                            </tr>
+                        @else
+                            <tr>
+                                <th scope="row">{{ $user->id }}</th>
+                                <td>{{ $user->name }}</td>
+                                <td>{{ $user->email }}</td>
                                 <td>
-                                    <select class="form-select" aria-label="Default select example"
-                                        wire:model='edit_role'>
-                                        <option value="0" {{ $edit_role == 0 ? 'selected' : '' }}>Admin</option>
-                                        <option value="1" {{ $edit_role == 1 ? 'selected' : '' }}>Inventory Clerk
-                                        </option>
-                                    </select>
-
+                                    @if ($user->role == 0)
+                                        <span class="text-danger">Admin</span>
+                                    @else
+                                        <span class="text-warning">Inventory Clerk</span>
+                                    @endif
                                 </td>
-                                @error('edit_role')
-                                    <span class="text-danger">{{ $message }}</span>
-                                @enderror
-                                <td>
-                                    <button wire:click='cancel_edit' class="btn btn-warning">Cancel</button>
-                                    <button wire:click='save_edit({{$user->id}})' class="btn btn-success">Save</button>
+                                <td class="d-flex justify-content-evenly">
+                                    <button wire:click='edit_user({{ $user->id }})' class="btn btn-success mx-1">Edit</button>
+                                    <button class="btn btn-danger mx-1">Delete</button>
                                 </td>
-                            </form>
-
-                        </tr>
-                    @else
-                        <tr>
-                            <th scope="row">{{ $user->id }}</th>
-                            <td>{{ $user->name }}</td>
-                            <td>{{ $user->email }}</td>
-                            <td>
-                                @if ($user->role == 0)
-                                    <span class="text-danger">Admin</span>
-                                @else
-                                    <span class="text-warning">Inventory Clerk</span>
-                                @endif
-                            </td>
-                            <td>
-                                <button wire:click='edit_user({{ $user->id }})'
-                                    class="btn btn-success">Edit</button>
-                                <button class="btn btn-danger">Delete</button>
-                            </td>
-                        </tr>
-                    @endif
-                @endforeach
+                            </tr>
+                        @endif
+                    @endforeach
 
 
-            </tbody>
-        </table>
+                </tbody>
+            </table>
+        </div>
 
     </div>
 </div>
