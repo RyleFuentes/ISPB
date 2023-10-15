29ee407ac5f107892dc198e1fdb54eb308fcceb6
oliverkean <20215296@s.ubaguio.edu>
2023-10-15 23:10:40 +0800

    dashboard sample

--- resources/views/layout/sidebar.blade.php
+++ resources/views/layout/sidebar.blade.php
@@ -22,7 +22,7 @@
         </a>
     </div>
 
-    <div class="d-flex flex-column" style="position: absolute; bottom: 0">
+    <div class="d-flex flex-column" style="bottom: 0">
         <a href="#" class="item mb-3 p-3 mx-2 text-danger">
             <i class="fas fa-sign-out-alt me-1"></i>
             <span>Logout</span>
--- resources/views/livewire/pages/dashboard.blade.php
+++ resources/views/livewire/pages/dashboard.blade.php
@@ -3,7 +3,56 @@
         @include('layout.sidebar')         
         <div class="container-fluid px-4" id="dashboard">
             @include('layout.navbar')   
-            {{$slot}}
+            <div class="my-2 row g-3">
+
+                <div class="col col-md-4 col-xl-3">
+                    <div class="card bg-c-green order-card">
+                        <div class="card-block">
+                            <h4 class="m-b-20 fw-bold text-primary">SALES</h4>
+                            <h2 class="text-right">
+                                <i class="fa fa-tag me-2"></i><span>486</span>
+                            </h2>
+                            <p class="m-b-0 fw-bold">Total Sales: 351</p>
+                        </div>
+                    </div>
+                </div> 
+
+                <div class="col col-md-4 col-xl-3">
+                    <div class="card bg-c-green order-card">
+                        <div class="card-block">
+                            <h4 class="m-b-20 fw-bold text-primary">Orders</h4>
+                            <h2 class="text-right">
+                                <i class="fa fa-tag me-2"></i><span>486</span>
+                            </h2>
+                            <p class="m-b-0 fw-bold">Total Sales: 351</p>
+                        </div>
+                    </div>
+                </div> 
+
+                <div class="col col-md-4 col-xl-3">
+                    <div class="card bg-c-green order-card">
+                        <div class="card-block">
+                            <h4 class="m-b-20 fw-bold text-primary">Orders</h4>
+                            <h2 class="text-right">
+                                <i class="fa fa-tag me-2"></i><span>486</span>
+                            </h2>
+                            <p class="m-b-0 fw-bold">Total Sales: 351</p>
+                        </div>
+                    </div>
+                </div> 
+            
+                <div class="col col-md-4 col-xl-3">
+                    <div class="card bg-c-green order-card">
+                        <div class="card-block">
+                            <h4 class="m-b-20 fw-bold text-primary">Orders</h4>
+                            <h2 class="text-right">
+                                <i class="fa fa-tag me-2"></i><span>486</span>
+                            </h2>
+                            <p class="m-b-0 fw-bold">Total Sales: 351</p>
+                        </div>
+                    </div>
+                </div>     
+            </div>
         </div>
     </div>
 @endauth
