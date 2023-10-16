6502c3a74893ae3f471a22804bf15b17798c7329
oliverkean <20215296@s.ubaguio.edu>
2023-10-16 08:39:54 +0800

    test

--- resources/views/layout/navbar.blade.php
+++ resources/views/layout/navbar.blade.php
@@ -1,6 +1,10 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-transparent py-4">
     <div class="d-flex align-items-center">
         <i class="fas fa-align-justify primary-text fs-4 me-3 text-white" id="menu-toggle"></i>
-        <h2 class="fs-6 fw-bold m-0 text-white">Dashboard</h2>
+        <h2 class="fs-6 fw-bold m-0 text-white">
+            {{ request()->is('dashboard*') ? 'Dashboard' : '' }}
+            {{ request()->is('products*') ? 'Products' : '' }}
+            {{ request()->is('users*') ? 'Users' : '' }}
+        </h2>
     </div>
 </nav>
