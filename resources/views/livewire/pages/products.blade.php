7da53ea601a9e869052d8464a4817536dcabd685
oliverkean <20215296@s.ubaguio.edu>
2023-10-16 17:49:32 +0800

    maya ulit

--- resources/views/livewire/pages/products.blade.php
+++ resources/views/livewire/pages/products.blade.php
@@ -2,67 +2,47 @@
      @include('layout.sidebar')
      <div class="container-fluid px-4" id="dashboard">
          @include('layout.navbar')
-         <div class="overflow-auto">
-             <div class="container rounded p-3 mt-2" style="background-color: #87814f;">
-                 <div class="container-fluid d-flex overflow-auto">
-                     <div class="container">
-                         <form class="d-flex">
-                             <div class="form-floating px-1 w-100">
-                                 <input type="text" id="search" class="form-control " placeholder="...">
-                                 <label for="search">Search</label>
-                             </div>
-                         </form>
-                     </div>
+         <div class="p-3 d-flex justify-content-end">
+             <button class="btn btn-light btn-sm rounded-pill me-2" data-bs-toggle="modal" data-bs-target="#addBrand">
+                 <i class="fa fa-plus-circle me-2 text-primary"></i>Add Brand
+             </button>
+             <button class="btn btn-light btn-sm rounded-pill me-2" wire:click='addProductMode'>
+                 <i class="fa fa-plus-circle me-2 text-primary"></i>Add Products
+             </button>
+         </div>
 
-                     <div class="container d-flex align-items-center " style="justify-content: end">
-                         <div class="row">
-                             <div class="col-sm-12 col-md-6 g-2">
-                                 <button class="btn btn-outline-light btn-sm" data-bs-toggle="modal"
-                                     data-bs-target="#addBrand">Brands</button>
-                                 <button class="btn btn-outline-light btn-sm"
-                                     wire:click='addProductMode'>Products</button>
-                             </div>
-                         </div>
-                     </div>
-                 </div>
-             </div>
+         @if (!$addProduct)
+             <div class="container">
+                     <div class="row row-cols-lg-3 row-cols-md-2 ">
+                         @foreach ($products as $product)
+                             <div class="col d-flex justify-content-around">
+                                 <div class="card m-1" style="width: 14rem;">
+                                     <img src="{{ Storage::url($product->product_image) }}" class="card-img-top"
+                                         alt="...">
+                                     <div class="card-body">
+                                         <p class="card-text">Product name: {{ $product->product_name }}</p>
+                                         <p class="card-text">Quantity: {{ $product->quantity }}</p>
+                                         <p class="card-text">Retail Price: {{ $product->retail_price }}</p>
+                                         <p class="card-text">Wholesale Price: {{ $product->wholesale_price }}</p>
+                                         <p class="card-text">Brand: {{ optional($product->brand)->brand_name }}.
+                                         </p>
 
-             @if (!$addProduct)
-                 <div class="container mt-2 rounded p-2 mb-2" style=" background-color: #f2f3f7">
-                     <div class="container">
-                         <div class="row row-cols-lg-3 row-cols-md-2 ">
-                             @foreach ($products as $product)
-                                 <div class="col d-flex justify-content-around">
-                                     <div class="card m-1" style="width: 14rem;">
-                                         <img src="{{ Storage::url($product->product_image) }}" class="card-img-top"
-                                             alt="...">
-                                         <div class="card-body">
-                                             <p class="card-text">Product name: {{ $product->product_name }}</p>
-                                             <p class="card-text">Quantity: {{ $product->quantity }}</p>
-                                             <p class="card-text">Retail Price: {{ $product->retail_price }}</p>
-                                             <p class="card-text">Wholesale Price: {{ $product->wholesale_price }}</p>
-                                             <p class="card-text">Brand: {{ optional($product->brand)->brand_name }}.
-                                             </p>
-
-                                             <div>
-                                                 <button class="btn btn-danger btn-sm">Delete</button>
-                                                 <button class="btn btn-warning btn-sm">Edit</button>
-                                             </div>
+                                         <div>
+                                             <button class="btn btn-danger btn-sm">Delete</button>
+                                             <button class="btn btn-warning btn-sm">Edit</button>
                                          </div>
                                      </div>
                                  </div>
-                             @endforeach
-                         </div>
+                             </div>
+                         @endforeach
                      </div>
                  </div>
-             @else
-                 @include('livewire.components.add_products_component')
-             @endif
+         @else
+             @include('livewire.components.add_products_component')
+         @endif
 
 
-             @include('livewire.modals.add-brand-modal')
-
-         </div>
+         @include('livewire.modals.add-brand-modal')
      </div>
  </div>
 
--- resources/views/livewire/pages/users.blade.php
+++ resources/views/livewire/pages/users.blade.php
@@ -3,92 +3,75 @@
         @include('layout.sidebar')
         <div class="container-fluid px-4" id="dashboard">
             @include('layout.navbar')
-            <div class="container-fluid d-flex ">
-                    <div class="container">
-                        <form class="d-flex">
-                            <div class="form-floating px-1 w-50">
-                                <input type="text" id="search" class="form-control " placeholder="...">
-                                <label for="search">Search</label>
-                            </div>
-                        </form>
-                    </div>
-                </div>
+            <table class="table">
+                <thead>
+                    <tr class="text-center fs-6">
+                        <td scope="col">ID</td>
+                        <td scope="col">Name</td>
+                        <td scope="col">Email</td>
+                        <td scope="col">Role</td>
+                        <td scope="col">Actions</td>
+                    </tr>
+                </thead>
+                <tbody class="text-center">
+                    @foreach ($users as $user)
+                        @if ($editing == true && $user->id == $editing_id)
+                            <tr>
+                                <form>
+                                    <th scope="row">{{ $user->id }}</th>
 
-            <div class="container mt-2 rounded p-3 border">
-                <div class="table-responsive">
-                    <table class="table">
-                        <thead>
-                            <tr class="text-center">
-                                <th scope="col">ID</th>
-                                <th scope="col">Name</th>
-                                <th scope="col">Email</th>
-                                <th scope="col">Role</th>
-                                <th scope="col">Actions</th>
+                                    <td><input wire:model='edit_name' type="text" class="form-control"
+                                            value="{{ $user->name }}"></td>
+                                    @error('edit_name')
+                                        <span class="text-danger">{{ $message }}</span>
+                                    @enderror
+                                    <td><input wire:model='edit_email' type="email" class="form-control"
+                                            value="{{ $user->email }}"></td>
+                                    @error('edit_email')
+                                        <span class="text-danger">{{ $message }}</span>
+                                    @enderror
+                                    <td>
+                                        <select class="form-select" aria-label="Default select example"
+                                            wire:model='edit_role'>
+                                            <option value="0" {{ $edit_role == 0 ? 'selected' : '' }}>Admin
+                                            </option>
+                                            <option value="1" {{ $edit_role == 1 ? 'selected' : '' }}>
+                                                Inventory Clerk
+                                            </option>
+                                        </select>
+                                    </td>
+                                    @error('edit_role')
+                                        <span class="text-danger">{{ $message }}</span>
+                                    @enderror
+                                    <td class="d-flex justify-content-evenly">
+                                        <button wire:click='cancel_edit' class="btn btn-warning mx-1">Cancel</button>
+                                        <button wire:click='save_edit({{ $user->id }})'
+                                            class="btn btn-success mx-1">Save</button>
+                                    </td>
+                                </form>
                             </tr>
-                        </thead>
-                        <tbody class="text-center">
-                            @foreach ($users as $user)
-                                @if ($editing == true && $user->id == $editing_id)
-                                    <tr>
-                                        <form>
-                                            <th scope="row">{{ $user->id }}</th>
-
-                                            <td><input wire:model='edit_name' type="text" class="form-control"
-                                                    value="{{ $user->name }}"></td>
-                                            @error('edit_name')
-                                                <span class="text-danger">{{ $message }}</span>
-                                            @enderror
-                                            <td><input wire:model='edit_email' type="email" class="form-control"
-                                                    value="{{ $user->email }}"></td>
-                                            @error('edit_email')
-                                                <span class="text-danger">{{ $message }}</span>
-                                            @enderror
-                                            <td>
-                                                <select class="form-select" aria-label="Default select example"
-                                                    wire:model='edit_role'>
-                                                    <option value="0" {{ $edit_role == 0 ? 'selected' : '' }}>Admin
-                                                    </option>
-                                                    <option value="1" {{ $edit_role == 1 ? 'selected' : '' }}>
-                                                        Inventory Clerk
-                                                    </option>
-                                                </select>
-                                            </td>
-                                            @error('edit_role')
-                                                <span class="text-danger">{{ $message }}</span>
-                                            @enderror
-                                            <td class="d-flex justify-content-evenly">
-                                                <button wire:click='cancel_edit'
-                                                    class="btn btn-warning mx-1">Cancel</button>
-                                                <button wire:click='save_edit({{ $user->id }})'
-                                                    class="btn btn-success mx-1">Save</button>
-                                            </td>
-                                        </form>
-                                    </tr>
-                                @else
-                                    <tr>
-                                        <th scope="row">{{ $user->id }}</th>
-                                        <td>{{ $user->name }}</td>
-                                        <td>{{ $user->email }}</td>
-                                        <td>
-                                            @if ($user->role == 0)
-                                                <span class="text-danger">Admin</span>
-                                            @else
-                                                <span class="text-warning">Inventory Clerk</span>
-                                            @endif
-                                        </td>
-                                        <td class="d-flex justify-content-evenly">
-                                            <button wire:click='edit_user({{ $user->id }})'
-                                                class="btn btn-success mx-1">Edit</button>
-                                            <button class="btn btn-danger mx-1">Delete</button>
-                                        </td>
-                                    </tr>
-                                @endif
-                            @endforeach
-                        </tbody>
-                    </table>
-                </div>
-
-            </div>
+                        @else
+                            <tr>
+                                <th scope="row">{{ $user->id }}</th>
+                                <td>{{ $user->name }}</td>
+                                <td>{{ $user->email }}</td>
+                                <td>
+                                    @if ($user->role == 0)
+                                        <span class="text-danger">Admin</span>
+                                    @else
+                                        <span class="text-warning">Inventory Clerk</span>
+                                    @endif
+                                </td>
+                                <td class="d-flex justify-content-evenly">
+                                    <button wire:click='edit_user({{ $user->id }})'
+                                        class="btn btn-success mx-1">Edit</button>
+                                    <button class="btn btn-danger mx-1">Delete</button>
+                                </td>
+                            </tr>
+                        @endif
+                    @endforeach
+                </tbody>
+            </table>
         </div>
     </div>
 </div>
