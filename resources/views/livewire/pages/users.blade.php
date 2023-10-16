e604fd8427225ca99bded0d7d9f6c61c9bdd8276
oliverkean <20215296@s.ubaguio.edu>
2023-10-17 04:01:00 +0800

    yow

--- resources/views/livewire/pages/users.blade.php
+++ resources/views/livewire/pages/users.blade.php
@@ -3,75 +3,87 @@
         @include('layout.sidebar')
         <div class="container-fluid px-4" id="dashboard">
             @include('layout.navbar')
-            <table class="table">
-                <thead>
-                    <tr class="text-center fs-6">
-                        <td scope="col">ID</td>
-                        <td scope="col">Name</td>
-                        <td scope="col">Email</td>
-                        <td scope="col">Role</td>
-                        <td scope="col">Actions</td>
-                    </tr>
-                </thead>
-                <tbody class="text-center">
-                    @foreach ($users as $user)
-                        @if ($editing == true && $user->id == $editing_id)
-                            <tr>
-                                <form>
-                                    <th scope="row">{{ $user->id }}</th>
+            <div class="card d-flex justify-content-center align-items-center mt-5">
+                <table class="table mt-5">
+                    <thead>
+                        <tr class="text-center fs-6">
+                            <td scope="col" class="text-primary">ID</td>
+                            <td scope="col" class="text-primary">Name</td>
+                            <td scope="col" class="text-primary">Email</td>
+                            <td scope="col" class="text-primary">Role</td>
+                            <td scope="col" class="text-primary">Actions</td>
+                        </tr>
+                    </thead>
+                    <tbody class="text-center">
+                        @foreach ($users as $user)
+                            @if ($editing == true && $user->id == $editing_id)
+                                <tr>
+                                    <form>
+                                        <th scope="row">{{ $user->id }}</th>
 
-                                    <td><input wire:model='edit_name' type="text" class="form-control"
-                                            value="{{ $user->name }}"></td>
-                                    @error('edit_name')
-                                        <span class="text-danger">{{ $message }}</span>
-                                    @enderror
-                                    <td><input wire:model='edit_email' type="email" class="form-control"
-                                            value="{{ $user->email }}"></td>
-                                    @error('edit_email')
-                                        <span class="text-danger">{{ $message }}</span>
-                                    @enderror
+                                        <td><input wire:model='edit_name' type="text" class="form-control"
+                                                value="{{ $user->name }}"></td>
+                                        @error('edit_name')
+                                            <span class="text-danger">{{ $message }}</span>
+                                        @enderror
+                                        <td><input wire:model='edit_email' type="email" class="form-control"
+                                                value="{{ $user->email }}"></td>
+                                        @error('edit_email')
+                                            <span class="text-danger">{{ $message }}</span>
+                                        @enderror
+                                        <td>
+                                            <select class="form-select" aria-label="Default select example"
+                                                wire:model='edit_role'>
+                                                <option value="0" {{ $edit_role == 0 ? 'selected' : '' }}>Admin
+                                                </option>
+                                                <option value="1" {{ $edit_role == 1 ? 'selected' : '' }}>
+                                                    Inventory Clerk
+                                                </option>
+                                            </select>
+                                        </td>
+                                        @error('edit_role')
+                                            <span class="text-danger">{{ $message }}</span>
+                                        @enderror
+                                        <td class="text-center">
+                                            <a wire:click='cancel_edit' class="text-danger mx-1"  style="cursor: pointer">
+                                                <i class="fas fa-cancel fs-5"></i>
+                                            </a>
+                                            <a wire:click='save_edit({{ $user->id }})'
+                                                class="mx-1 text-success" style="cursor: pointer">
+                                                <i class="fas fa-save fs-5"></i>
+                                            </a>
+                                        </td>
+                                    </form>
+                                </tr>
+                            @else
+                                <tr>
+                                    <th scope="row">{{ $user->id }}</th>
+                                    <td>{{ $user->name }}</td>
+                                    <td>{{ $user->email }}</td>
                                     <td>
-                                        <select class="form-select" aria-label="Default select example"
-                                            wire:model='edit_role'>
-                                            <option value="0" {{ $edit_role == 0 ? 'selected' : '' }}>Admin
-                                            </option>
-                                            <option value="1" {{ $edit_role == 1 ? 'selected' : '' }}>
-                                                Inventory Clerk
-                                            </option>
-                                        </select>
+                                        @if ($user->role == 0)
+                                            <span class="text-danger">Admin</span>
+                                        @else
+                                            <span class="text-warning">Inventory Clerk</span>
+                                        @endif
                                     </td>
-                                    @error('edit_role')
-                                        <span class="text-danger">{{ $message }}</span>
-                                    @enderror
-                                    <td class="d-flex justify-content-evenly">
-                                        <button wire:click='cancel_edit' class="btn btn-warning mx-1">Cancel</button>
-                                        <button wire:click='save_edit({{ $user->id }})'
-                                            class="btn btn-success mx-1">Save</button>
+                                    <td class="text-center">
+                                        <a wire:click='edit_user({{ $user->id }})'
+                                            class="mx-1 text-primary" style="cursor: pointer">
+                                            <i class="fas fa-edit"></i>
+                                        </a>
+                                        <a class="text-danger mx-1" style="cursor: pointer">
+                                            <i class="fas fa-trash"></i>
+                                        </a>
                                     </td>
-                                </form>
-                            </tr>
-                        @else
-                            <tr>
-                                <th scope="row">{{ $user->id }}</th>
-                                <td>{{ $user->name }}</td>
-                                <td>{{ $user->email }}</td>
-                                <td>
-                                    @if ($user->role == 0)
-                                        <span class="text-danger">Admin</span>
-                                    @else
-                                        <span class="text-warning">Inventory Clerk</span>
-                                    @endif
-                                </td>
-                                <td class="d-flex justify-content-evenly">
-                                    <button wire:click='edit_user({{ $user->id }})'
-                                        class="btn btn-success mx-1">Edit</button>
-                                    <button class="btn btn-danger mx-1">Delete</button>
-                                </td>
-                            </tr>
-                        @endif
-                    @endforeach
-                </tbody>
-            </table>
+                                </tr>
+                            @endif
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
         </div>
     </div>
 </div>
+
+{{--  --}}
