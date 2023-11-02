0286cba6453473cda099a11d0657be1b1084de3b
Vincent james <Vincent James Beato>
2023-11-02 09:32:54 +0800

    simple profile v.2

--- resources/views/livewire/components/profile_information.blade.php
+++ resources/views/livewire/components/profile_information.blade.php
@@ -1,7 +1,7 @@
-<div>
+<div class="row">
     <!-- ala pa ako maisip na ilagay, lakas ng mental block -->
-    <div class="d-flex align-items-center justify-content-center">
-        <div class="bg-primary rounded py-3 px-5 bg-opacity-25 w-75">
+    <div class="col-md-5 d-flex align-items-center justify-content-center">
+        <div class="bg-primary rounded py-3 px-5 bg-opacity-25 w-100">
             <div class="d-flex align-items-center justify-content-center py-1">
                 <div class="prof rounded bg-secondary">
                     <img class="img prof" src="{{ asset('images/logo.png') }}" alt="logo">
@@ -9,38 +9,38 @@
             </div>
             <hr>
             <div class="px-3">
-                <p class="px-3 fs-3 text-center">{{ $user->name }}</p>
+                <p class="px-3 fs-4 text-center">{{ $user->name }}</p>
                 @if ($user->role == 0)
-                    <p class="px-3 fs-5 text-center">Admin</p>
+                    <p class="px-3 fs-6 text-center">Admin</p>
                 @else
-                    <p class="px-3 fs-4">Inventory Clerk</p>
+                    <p class="px-3 fs-6">Inventory Clerk</p>
                 @endif
             </div>
         </div>
     </div>
 
-    <div class="mt-3 d-flex justify-content-center">
-        <div class="bg-primary rounded px-5 pt-5 bg-opacity-25 w-75">
+    <div class="col-md-7 mt-3 d-flex justify-content-center">
+        <div class="bg-primary rounded px-5 pt-5 bg-opacity-25 w-100 h-auto">
             <h2 class="fs-2">ABOUT</h2>
             <hr />
-            <small class="badge rounded-pill bg-primary fs-4">Email</small>
+            <small class="badge rounded-pill bg-primary fs-5">Email</small>
             <p class="px-3 fs-5">{{ $user->email }}</p>
-            <small class="badge rounded-pill bg-primary fs-4">Address</small>
+            <small class="badge rounded-pill bg-primary fs-6">Address</small>
             <p class="px-3 fs-5">Muntinlupa City</p>
-            <small class="badge rounded-pill bg-primary fs-4">Contact</small>
+            <small class="badge rounded-pill bg-primary fs-6">Contact</small>
             <p class="px-3 fs-5">09**-***-****</p>
         </div>
     </div>
 
     <div class="mt-3 d-flex justify-content-center">
-        <div class="bg-primary rounded px-5 pt-5 bg-opacity-25 w-75">
+        <div class="bg-primary rounded px-5 pt-5 bg-opacity-25 w-100">
             <h2 class="fs-2">IN CASE OF EMERGENCY</h2>
             <hr>
-            <small class="badge rounded-pill bg-primary fs-4">Guardian/Parent</small>
+            <small class="badge rounded-pill bg-primary fs-6">Guardian/Parent</small>
             <p class="px-3 fs-5">name of guardian or parent</p>
-            <small class="badge rounded-pill bg-primary fs-4">Address</small>
+            <small class="badge rounded-pill bg-primary fs-6">Address</small>
             <p class="px-3 fs-5">Muntinlupa City</p>
-            <small class="badge rounded-pill bg-primary fs-4">Emergency Contact</small>
+            <small class="badge rounded-pill bg-primary fs-6">Emergency Contact</small>
             <p class="px-3 fs-5">09**-***-****</p>
         </div>
     </div>
