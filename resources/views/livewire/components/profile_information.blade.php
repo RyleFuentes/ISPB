89fd1804a1a596b39e6f7c7f150e7226b7a533a2
Vincent james <Vincent James Beato>
2023-11-04 10:10:31 +0800

    testing

--- resources/views/livewire/components/profile_information.blade.php
+++ resources/views/livewire/components/profile_information.blade.php
@@ -3,17 +3,17 @@
     <div class="col-md-5 d-flex align-items-center justify-content-center">
         <div class="bg-primary rounded py-3 px-3 bg-opacity-25 w-100 h-100">
             <div class="d-flex align-items-center justify-content-center py-1">
-                <div class="prof rounded bg-primary bg-opacity-25 border border-3 border-light">
-                    <img class="img prof" src="{{ asset('images/logo.png') }}" alt="logo">
+                <div class="prof rounded bg-primary bg-opacity-25">
+                    <img class="img prof rounded border border-3 border-light" src="{{ asset('images/bean.jpg') }}" alt="logo">
                 </div>
             </div>
             <hr>
             <div>
-                <p class="fs-4 text-center">{{ $user->name }}</p>
+                <p class="fs-4 text-center mb-0">{{ $user->name }}</p>
                 @if ($user->role == 0)
-                    <p class="fs-6 text-center">Admin</p>
+                    <p class="fs-6 text-center mb-0">Admin</p>
                 @else
-                    <p class="fs-6 text-center">Inventory Clerk</p>
+                    <p class="fs-6 text-center mb-0">Inventory Clerk</p>
                 @endif
             </div>
         </div>
@@ -21,7 +21,7 @@
 
     <div class="col-md-7 mt-3 d-flex justify-content-center">
         <div class="bg-primary rounded px-5 pt-5 bg-opacity-25 w-100 h-100">
-            <h2 class="fs-2 text-primary">BASIC INFO</h2>
+            <h2 class="fs-2 fs-sm-2 text-primary">BASIC INFO</h2>
             <hr />
             <small class="badge rounded-pill bg-primary fs-6">Email</small>
             <p class="px-3 fs-5">{{ $user->email }}</p>
