d5323e25f2150e389cea4b7030b685573469a1f7
oliverkean <20215296@s.ubaguio.edu>
2023-11-05 22:20:34 +0800

    mg

--- resources/views/layout/navbar.blade.php
+++ resources/views/layout/navbar.blade.php
@@ -2,11 +2,11 @@
     <div class="d-flex align-items-center">
         <i class="fas fa-align-justify primary-text fs-4 me-3 text-primary" id="menu-toggle"></i>
         <h2 class="fs-6 fw-bold m-0 text-primary">
-            {{ request()->is('dashboard*') ? 'Dashboard' : '' }}
-            {{ request()->is('products*') ? 'Products' : '' }}
-             {{ request()->is('orders*') ? 'Orders' : '' }}
-            {{ request()->is('users*') ? 'Users' : '' }}
-            {{ request()->is('profile*') ? 'Profile' : '' }}
+            {{ request()->is('dashboard') ? 'Dashboard' : '' }}
+            {{ request()->is('products') ? 'Products' : '' }}
+             {{ request()->is('orders') ? 'Orders' : '' }}
+            {{ request()->is('users') ? 'Users' : '' }}
+            {{ request()->is('profile') ? 'Profile' : '' }}
         </h2>
     </div>
 
