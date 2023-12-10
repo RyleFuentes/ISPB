39122924bd4b5fe8e2d944930f26642e3f0ff99b
oliverkean <20215296@s.ubaguio.edu>
2023-12-10 21:17:36 +0800

    test

--- resources/views/livewire/pages/auth/register.blade.php
+++ resources/views/livewire/pages/auth/register.blade.php
@@ -49,7 +49,7 @@ new #[Layout('layouts.guest')] class extends Component
         <!-- Name -->
         <div class="form-floating mt-2">
             <x-text-input wire:model="name" id="name" class="block mt-1 w-full" type="text" name="name"
-                 autofocus autocomplete="name" placeholder="name" />
+                autofocus autocomplete="name" placeholder="name" />
             <x-input-label for="name" :value="__('Name')" />
 
             <x-input-error :messages="$errors->get('name')" class="mt-2" />
@@ -58,7 +58,7 @@ new #[Layout('layouts.guest')] class extends Component
         <!-- Email Address -->
         <div class="form-floating mt-3">
             <x-text-input wire:model="email" id="email" class="block mt-1 w-full" type="email" name="email"
-                 autocomplete="username" placeholder="email" />
+                autocomplete="username" placeholder="email" />
             <x-input-label for="email" :value="__('Email')" />
 
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
@@ -68,7 +68,7 @@ new #[Layout('layouts.guest')] class extends Component
         <div class="form-floating mt-3">
 
             <x-text-input wire:model="password" id="password" class="block mt-1 w-full" type="password" name="password"
-                 autocomplete="new-password" placeholder="password" />
+                autocomplete="new-password" placeholder="password" />
             <x-input-label for="password" :value="__('Password')" />
 
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
@@ -78,34 +78,35 @@ new #[Layout('layouts.guest')] class extends Component
         <div class="form-floating mt-3">
 
             <x-text-input wire:model="password_confirmation" id="password_confirmation" class="block mt-1 w-full"
-                type="password" name="password_confirmation"  autocomplete="new-password"
+                type="password" name="password_confirmation" autocomplete="new-password"
                 placeholder="password_confirmation" />
             <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
 
             <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
         </div>
 
-       
 
-        <div  class="flex items-center justify-center flex-column">
-            <x-primary-button wire:loading.remove class="flex items-center justify-center ms-3 mt-3 bg-primary w-100 rounded-pill">
+
+        <div class="flex items-center justify-center flex-column">
+            <x-primary-button wire:loading.remove
+                class="flex items-center justify-center ms-3 mt-3 bg-primary w-100 rounded-pill">
                 {{ __('Register') }}
-                
-            </x-primary-button> 
+
+            </x-primary-button>
 
             <div class="flex gap-2">
                 <div wire:loading class="spinner-grow text-dark mt-3" role="status">
-                    
+
                     <span class="visually-hidden">Loading...</span>
-                  </div>
-                  <div wire:loading class="spinner-grow text-primary mt-3" role="status">
-                    
+                </div>
+                <div wire:loading class="spinner-grow text-primary mt-3" role="status">
+
                     <span class="visually-hidden">Loading...</span>
-                  </div>
-                  <div wire:loading class="spinner-grow text-warning mt-3" role="status">
-                    
+                </div>
+                <div wire:loading class="spinner-grow text-warning mt-3" role="status">
+
                     <span class="visually-hidden">Loading...</span>
-                  </div>
+                </div>
             </div>
         </div>
 
@@ -117,6 +118,6 @@ new #[Layout('layouts.guest')] class extends Component
                 <a href="{{ route('login') }}" class="text-primary text-decoration-none fw-semibold"
                     wire:navigate>Login</a>
             </p>
-        </div>  
+        </div>
     </form>
 </div>
